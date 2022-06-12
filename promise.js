19aa831a2bb45ef15352b14cee3fbd7e496459a1
doman-tegeh <doman.tegeh@gmail.com>
2022-06-12 20:24:02 +0800

    update

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+  let kino0 = await promiseTheaterIXX();
+  let kino1 = await promiseTheaterVGC();
+  let kinos = kino0.concat(kino1);
+  let totalEmosi = 0;
+  kinos.map(kino0 => kino0.hasil == emosi && (totalEmosi += 1));
+  return totalEmosi;
+};
 
 module.exports = {
   promiseOutput,
